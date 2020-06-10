9bae782f275f2a697c23fa5cf8e20e611e0d3cfe
4480135 <4480135@RTS-365.dk>
2020-06-10 08:19:52 +0200

    Improve mobile header.

--- views/index/src/js/Header.js
+++ views/index/src/js/Header.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
 
 const Header = () => {
+    const [menuUp, setMenuUp] = useState(true);
+
+    const handleClick = (e) => {
+        if (e.target.tagName == "A" || e.target.className == "header-main") {
+            setMenuUp(!menuUp);
+
+            document.querySelector("header").style = !menuUp
+                ? "transform: translateY(calc(-100% + 56px))"
+                : "transform: none";
+        }
+    };
+
     return (
-        <header>
-            <span>MadCreativity</span>
+        <header onClick={handleClick}>
+            <div className="header-main">
+                <span className="header-title">MadCreativity</span>
+                <span className="swipe-down-text">Tap For Menu</span>
+            </div>
 
             <nav>
                 <Link to="/">Home</Link>
